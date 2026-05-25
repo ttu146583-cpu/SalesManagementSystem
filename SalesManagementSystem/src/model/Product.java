8693212fc9e9254b5816e570b5080d070f1c0558
ttu146583-cpu <ttu146583@gmail.com>
2026-05-25 19:51:39 +0700

    product

--- SalesManagementSystem/src/model/Product.java
+++ SalesManagementSystem/src/model/Product.java
@@ -20,12 +20,61 @@ public class Product {
     private int stockQuantity;
 
     // Constructor
-    public Product() {
-        
+    public Product(int idProduct, String nameProduct, String categoryProduct, double price, int stockQuantity) {
+        this.idProduct=idProduct;
+        this.nameProduct=nameProduct;
+        this.categoryProduct=categoryProduct;
+        this.price=price;
+        this.stockQuantity=stockQuantity; 
     }
-
-    public void UpdateStockProduct() {
-
+    //get để đọc 
+    //setter sửa giá trị
+    
+    
+    public int getidProduct() {
+        return idProduct;
+    }
+    public void setteridProduct(int idProduct){
+        if (idProduct > 0){
+            this.idProduct=idProduct;
+        }
+    }
+    public String getnameProduct(){
+        return nameProduct;
+    }
+    public void setternameProduct(String nameProduct){
+        if (nameProduct != null){ //khong de rong
+            this.nameProduct=nameProduct;
+        }
+    }
+    public String getcategoryProduct(){
+        return getcategoryProduct;
+    }
+    public void settercategoryProduct(String categoryProduct){
+        this.categoryProduct=categoryProduct;
+    }
+    public double getprice(){
+        return price;
+    }
+    public void setterprice (double price){
+        if (price > 0)
+        {
+            this.price=price;
+        }
+    }
+    public int getstockQuantity(){
+        return getstockQuantity;
+    }
+    public void setterstockQuantity(int stockQuantity){
+        if (stockQuantity > 0)
+        {
+            this.stockQuantity=stockQuantity;
+        }
+    }
+    //method
+    public void updateStockQuantity(int quantity) {
+        if (stockQuantity + quantity >= 0) //kho khong am
+        this.stockQuantity=stockQuantity;
     }
 }
 
